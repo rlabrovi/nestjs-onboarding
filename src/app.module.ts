89d7c183eba0b78ce6efc2dd16bcfa04704f2b6b
Roko Labrovic <roko.labrovic@decode.agency>
2023-01-31 10:21:36 +0100

    Starting book module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import psqlConfig from './typeorm-psql.config';
 import { UserModule } from './user/user.module';
 import { AuthorModule } from './author/author.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(psqlConfig), UserModule, AuthorModule],
+  imports: [TypeOrmModule.forRoot(psqlConfig), UserModule, AuthorModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
