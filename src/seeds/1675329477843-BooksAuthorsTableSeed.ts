0c012ec5d56df4f50cca2d3a0601dd054110743e
Roko Labrovic <roko.labrovic@decode.agency>
2023-02-08 13:43:36 +0100

    Adding BooksAuthorsTableSeed

---
+++ src/seeds/1675329477843-BooksAuthorsTableSeed.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class BooksAuthorsTableSeed1675329477843 implements MigrationInterface {
+  name = 'BooksAuthorsTableSeed1675329477843';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`INSERT INTO books_authors values (1, 1), (2, 2)`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return;
+  }
+}
